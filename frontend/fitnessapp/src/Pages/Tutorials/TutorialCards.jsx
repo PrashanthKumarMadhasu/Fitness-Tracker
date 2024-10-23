f1a59dc8da1ec356b5845463e781c24ee2d4328e
PrashanthKumarMadhasu <madhasuprashanth15@gmail.com>
2024-10-23 21:32:10 +0530

    tutorials added fd

---
+++ frontend/fitnessapp/src/Pages/Tutorials/TutorialCards.jsx
@@ -0,0 +1,107 @@
+import chestIcon from '../../Components/Assets/SvgFiles/chest.svg';
+import backIcon from '../../Components/Assets/SvgFiles/back.svg';
+import legsIcon from '../../Components/Assets/SvgFiles/legs.svg';
+import absIcon from '../../Components/Assets/SvgFiles/abs.svg';
+import shouldersIcon from '../../Components/Assets/SvgFiles/shoulders.svg';
+import armsIcon from '../../Components/Assets/SvgFiles/arms.svg';
+
+export const Categories = [
+    {
+        id: 'chest',
+        name: 'Chest',
+        icon:chestIcon,
+
+    },
+    {
+        id: 'back',
+        name: 'Back',
+        icon:backIcon,
+    },
+    {
+        id: 'legs',
+        name: 'Legs',
+        icon:legsIcon,
+    },
+    {
+        id: 'abs',
+        name: 'Abs',
+        icon:absIcon,
+    },
+    {
+        id: 'shoulders',
+        name: 'Shoulders',
+        icon:shouldersIcon,
+    },
+    {
+        id: 'arms',
+        name: 'Arms',
+        icon:armsIcon,
+    }
+    
+];
+
+export const videoData = {
+    chest: [
+      {
+        id: 1,
+        title: 'Upper Body Strength Training',
+        url: 'https://www.youtube.com/embed/YcmTJQS04VE?controls=0&modestbranding=1&autoplay=0&rel=0&showinfo=0',
+        type: 'instagram',
+        description: 'Focus on chest, back, and arms with this upper body workout.'
+      },
+      {
+        id: 2,
+        title: 'Effective Upper Body Exercises',
+        url: 'https://youtube.com/shorts/ZaTM37cfiDs?si=5M7mMuzDh7R3lbqw',
+        type: 'youtube',
+        description: 'A great mix of exercises to build upper body strength.'
+      }
+    ],
+    back: [
+      {
+        id: 1,
+        title: 'Deadlift',
+        url: 'https://youtube.com/shorts/ZaTM37cfiDs?si=s3lLYqbGhk2Zrlvb',
+      },
+      {
+        id: 2,
+        title: 'Pull-ups',
+        url: 'https://youtube.com/shorts/ZPG8OsHKXLw?si=Ke3vb2E9HE-Xdl2t',
+      },
+      {
+        id: 3,
+        title: 'Bent-Over Row',
+        url: 'https://youtube.com/shorts/Nqh7q3zDCoQ?si=Dx4_SNFYYICpkuSd',
+      },
+      {
+        id: 4,
+        title: 'Lat Pulldown',
+        url: 'https://youtube.com/shorts/aaAb20857Fs?si=RnSc_eyJQKxVxsvA',
+      },
+      {
+        id: 5,
+        title: 'T-Bar Row"',
+        url: 'https://youtube.com/shorts/Ko9oyL1-Ojs?si=tmtaTgoLa-LJmQQF',
+      },
+      {
+        id: 6,
+        title: 'Romanian Deadlifts',
+        url: 'https://youtube.com/shorts/rjRfOYWGeVg?si=2nduyTf3IGrgojPq',
+      },
+      {
+        id: 6,
+        title: 'Romanian Deadlifts',
+        url: 'https://youtube.com/shorts/rjRfOYWGeVg?si=2nduyTf3IGrgojPq',
+      },
+      {
+        id: 6,
+        title: 'Romanian Deadlifts',
+        url: 'https://youtube.com/shorts/rjRfOYWGeVg?si=2nduyTf3IGrgojPq',
+      },
+      {
+        id: 6,
+        title: 'Romanian Deadlifts',
+        url: 'https://youtube.com/shorts/rjRfOYWGeVg?si=2nduyTf3IGrgojPq',
+      },
+    ]
+  };
--- frontend/fitnessapp/src/Pages/Tutorials/Tutorials.jsx
+++ frontend/fitnessapp/src/Pages/Tutorials/Tutorials.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Tutorials.css';
+import { Categories, videoData } from './TutorialCards';
+import YoutubeVideo from './YoutubeVideo';
+
 
 const Tutorials = () => {
-  return (
-    <div>Tutorials</div>
-  )
-}
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-export default Tutorials
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleBackClick = () => {
+    setSelectedCategory(null);
+  };
+
+  return (
+    <div className='tutorial-container'>
+      {!selectedCategory ? (
+        <>
+          <h1 className='tutorial-header'>Exercise Categories</h1>
+          <div className='card-grid'>
+            {Categories.map((category, index) => (
+              <div
+                key={index}
+                className='category-card'
+                onClick={() => handleCategoryClick(category.id)}
+              >
+                <h3>{category.name}</h3>
+                <img src={category.icon} alt={category.name} className='img-shadow'></img>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className='video-section'>
+          <button className='back-button' onClick={handleBackClick}>
+            Back to Categories
+          </button>
+          <h2>{Categories.find(cat => cat.id === selectedCategory).name} Videos</h2>
+          <div className='video-list'>
+            {videoData[selectedCategory].map((video, index) => (
+              <div key={index} className='video-frame'>
+                {/* <h3>{video.title}</h3> */}
+                <YoutubeVideo url={`${((video.url).split('?')[0]).replace('shorts','embed')}?controls=0&modestbranding=1&autoplay=0&rel=0&showinfo=0`}/>
+                <p className='workout-name'>{video.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tutorials;
