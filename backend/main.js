2f83641d9eb7b97d1c6cbef77cd724c48aef6a5e
PrashanthKumarMadhasu <madhasuprashanthkumar15@gmail.com>
2024-11-05 13:14:22 +0530

    frontend final deployment

--- backend/main.js
+++ backend/main.js
@@ -7,8 +7,7 @@ require('dotenv').config();
 
 // CORS Setup
 const allowedOrigins = [
-  'https://fitnessapp-fitnesttracker.netlify.app',
-  'https://fitnest-eta.vercel.app',
+  'https://fitnestappz.netlify.app/',
   'http://localhost:5173',
   'http://127.0.0.1:5173'
 ];
