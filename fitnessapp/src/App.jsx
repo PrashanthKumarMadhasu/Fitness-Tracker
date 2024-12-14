8419267c7f8cc721a909cd0246de67e2f6ef1abb
PrashanthKumarMadhasu <madhasuprashanth15@gmail.com>
2024-12-14 15:15:20 +0530

    welcome toast added

--- fitnessapp/src/App.jsx
+++ fitnessapp/src/App.jsx
@@ -1,8 +1,8 @@
 import { ThemeProvider, styled } from "styled-components";
-import { lightTheme,darkTheme } from "./Utils/Theme";
+import { lightTheme, darkTheme } from "./Utils/Theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Authentication from "./Pages/Authentication";
-import { useState, useEffect, useContext} from "react";
+import { useState, useEffect, useContext } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "./Components/Navbar";
 import Dashboard from "./Pages/Dashboard";
@@ -14,18 +14,21 @@ import Contact from "./Pages/Contact/Contact";
 import SplashScreen from "./Components/SplashScreen/SplashScreen";
 import ForgotForm from "./Components/ForgotPassword/ForgotForm";
 import Dropdowns from "./Components/Cards/Dropdowns";
-import {ThemeContext} from './Utils/ThemeContext';
-import BackImage from './Components/Assets/dark.jpg';
+import { ThemeContext } from "./Utils/ThemeContext";
+import BackImage from "./Components/Assets/dark.jpg";
+import { Toaster, toast } from 'sonner';
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
-  background-image: url(${({ theme }) => (theme.theme==='true'?BackImage:'')});
-  background-color:${({ theme }) => (theme.theme==='true'?'none':'#f3efe9f5')};
+  background-image: url(${({ theme }) =>
+    theme.theme === "true" ? BackImage : ""});
+  background-color: ${({ theme }) =>
+    theme.theme === "true" ? "none" : "#f3efe9f5"};
   background-repeat: no-repeat;
-  background-size:cover;
+  background-size: cover;
   background-position: center;
   color: ${({ theme }) => theme.text_primary};
   overflow-x: hidden;
@@ -34,39 +37,66 @@ const Container = styled.div`
 `;
 
 function App() {
-
   const { currentUser } = useSelector((state) => state.user);
   const [showSplash, setShowSplash] = useState(false);
   const { themeColor, setThemeColor } = useContext(ThemeContext);
-  
+
   useEffect(() => {
     if (currentUser) {
       setShowSplash(true);
       const timer = setTimeout(() => {
         setShowSplash(false);
+        toast(`Welcome ${currentUser.name}👋`, {
+          style: {
+            color: "green",       
+            background: "none",  
+            padding: "5px",  
+            borderRadius: "8px",
+            margin: "50px 0 0 0 ",
+            fontSize:"18px",
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+          position: "top-center",
+          
+        });
       }, 3000);
       return () => clearTimeout(timer);
     }
   }, [currentUser]);
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", themeColor ? "dark" : "light");
+    document.documentElement.setAttribute(
+      "data-theme",
+      themeColor ? "dark" : "light"
+    );
   }, [themeColor]);
 
   return (
-    <ThemeProvider theme={themeColor? darkTheme:lightTheme}>
+    <ThemeProvider theme={themeColor ? darkTheme : lightTheme}>
+      <Toaster />
       <BrowserRouter>
         {currentUser ? (
           showSplash ? (
             <SplashScreen />
           ) : (
-            <Container >
+            <Container>
               <Navbar currentUser={currentUser} />
               <Routes>
-                <Route path="/" exact element={<Dashboard currentUser={currentUser} />} />
+                <Route
+                  path="/"
+                  exact
+                  element={<Dashboard currentUser={currentUser} />}
+                />
                 <Route path="/history" exact element={<History />} />
                 <Route path="/tutorials" exact element={<Tutorials />} />
-                <Route path="/launchworkout" exact element={<LaunchWorkout />} />
+                <Route
+                  path="/launchworkout"
+                  exact
+                  element={<LaunchWorkout />}
+                />
                 <Route path="/bmi" exact element={<Bmi />} />
                 <Route path="/contact" exact element={<Contact />} />
                 <Route path="/dropdown" exact element={<Dropdowns />} />
@@ -86,4 +116,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
